e9e7fac54045cdbae41c4067cc56659a5c22149d
Sheryl Boles <sheryl.boles@gmail.com>
2023-10-24 20:04:42 -0400

    initial data

--- Models/User.cs
+++ Models/User.cs
@@ -3,9 +3,11 @@
     public class User
     {
         public int UserId { get; set; }
-        public string? FireName { get; set; }
+        public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Bio { get; set; }
         public Mentor? Mentor { get; set; }
+        public int MentorId { get; set; }
+        
     }
 }
