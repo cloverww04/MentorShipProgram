eeb8b8ec49d1510e3653aba6d41dae1809da2c20
Nathan Clover <cloverww04@gmail.com>
2023-10-24 19:12:35 -0500

    commit endpoints

--- Models/User.cs
+++ Models/User.cs
@@ -5,12 +5,10 @@
         public int UserId { get; set; }
         public string? FirstName { get; set; }
         public string? UID { get; set; }
-        public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Bio { get; set; }
         public int MentorId { get; set; }
         public Mentor? Mentor { get; set; }
-        public int MentorId { get; set; }
 
     }
 }
